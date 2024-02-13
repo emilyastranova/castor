3f3b0dc6c18a74d0f38002cc4349e2402f45a843
emilyastranova <emily@astralabs.dev>
2024-02-12 20:28:52 -0600

    Add jobs to dashboard
    Signed-off-by: emilyastranova <emily@astralabs.dev>

--- frontend/src/routes/+page.server.js
+++ frontend/src/routes/+page.server.js
@@ -1,6 +1,15 @@
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ fetch }) {
-    const response = await fetch(`/api/v1/tasks`);
-    const data = await response.json();
+    // Get tasks data
+    const tasks = await fetch(`/api/v1/tasks`);
+    const tasks_data = await tasks.json();
+    // Get jobs data
+    const jobs = await fetch(`/api/v1/jobs`);
+    const jobs_data = await jobs.json();
+    // Combine data
+    const data = {
+        tasks: tasks_data,
+        jobs: jobs_data
+    };
     return { data };
-    }
+}
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,21 +2,38 @@
 	import { Content, Grid, Row, Column } from "carbon-components-svelte";
 	import { BarChartSimple } from "@carbon/charts-svelte";
 	export let data;
-	let tasks = data.data;
+	let tasks = data.data.tasks;
+	let jobs = data.data.jobs;
 	// Make a dictionary to hold the counts of each status
-	let status_counts = {};
+	let status_counts_tasks = {};
 	// Loop through the tasks and count the status
 	tasks.forEach((task) => {
-		if (status_counts[task.status]) {
-			status_counts[task.status] += 1;
+		if (status_counts_tasks[task.status]) {
+			status_counts_tasks[task.status] += 1;
 		} else {
-			status_counts[task.status] = 1;
+			status_counts_tasks[task.status] = 1;
 		}
 	});
 	// Create a new array of objects to hold the status and count
 	let status_counts_array = [];
-	Object.keys(status_counts).forEach((key) => {
-		status_counts_array.push({ group: key, value: status_counts[key] });
+	Object.keys(status_counts_tasks).forEach((key) => {
+		status_counts_array.push({ group: key, value: status_counts_tasks[key] });
+	});
+
+		// Make a dictionary to hold the counts of each status
+		let status_counts_jobs = {};
+	// Loop through the jobs and count the status
+	jobs.forEach((task) => {
+		if (status_counts_jobs[task.status]) {
+			status_counts_jobs[task.status] += 1;
+		} else {
+			status_counts_jobs[task.status] = 1;
+		}
+	});
+	// Create a new array of objects to hold the status and count
+	let status_counts_array_jobs = [];
+	Object.keys(status_counts_jobs).forEach((key) => {
+		status_counts_array_jobs.push({ group: key, value: status_counts_jobs[key] });
 	});
 
 </script>
@@ -26,8 +43,33 @@
 		<Row>
 			<Column>
 				<h1>Dashboard</h1>
+			</Column>
+		</Row>
+		<br/>
+		<Row>
+			<Column>
+				<h2>Tasks</h2>
 				<BarChartSimple
 					data={status_counts_array}
+					options={{
+						theme: "g100",
+						title: "Tasks by Category",
+						height: "400px",
+						axes: {
+							left: { mapsTo: "value" },
+							bottom: { mapsTo: "group", scaleType: "labels" },
+						},
+                        data:
+                        {
+                            loading: false
+                        }
+					}}
+				/>
+			</Column>
+			<Column>
+				<h2>Jobs</h2>
+				<BarChartSimple
+					data={status_counts_array_jobs}
 					options={{
 						theme: "g100",
 						title: "Tasks by Category",
