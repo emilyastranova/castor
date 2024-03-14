94367f4860b1ae514b101bf328a227184d980eae
emilyastranova <emily@astralabs.dev>
2024-03-14 01:10:55 -0500

    Progress in feed mockup
    Signed-off-by: emilyastranova <emily@astralabs.dev>

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,25 +1,46 @@
 <script>
 	import { Content, Grid, Row, Column, ClickableTile, Tag, TextArea, Button } from "carbon-components-svelte";
 	import SendAlt from "carbon-icons-svelte/lib/SendAlt.svelte";
+	import Hashtag from "carbon-icons-svelte/lib/Hashtag.svelte";
+	import Image from "carbon-icons-svelte/lib/Image.svelte";
+	import CheckboxIndeterminate from "carbon-icons-svelte/lib/CheckboxIndeterminate.svelte";
+	import Link from "carbon-icons-svelte/lib/Link.svelte";
 
 </script>
 
 <Content>
 	<Grid>
 		<Row>
-			<Column>
-				<h1>Dashboard</h1>
-			</Column>
-		</Row>
-		<br/>
-		<Row>
 			<Column sm={{ span: 3}}>
-				<h2>Feed</h2>
+				<h2>Activity Feed</h2>
 				<!-- Create post with text box -->
-				<TextArea id="post" labelText="Create a post" placeholder="Enter your post here" />
-				<Button icon={SendAlt} kind="primary">Send</Button>
+				<TextArea class="mb-2" id="post" labelText="Create a post" placeholder="Enter your post here" />
+				<!-- Spread buttons to far left and right -->
+				<div class="flex justify-between">
+					<div class="flex">
+						<Button icon={Hashtag} kind="ghost" iconDescription="Tags" />
+						<Button icon={Image} kind="ghost" iconDescription="Add Image" />
+						<Button icon={CheckboxIndeterminate} kind="ghost" iconDescription="Content Syntax" />
+						<Button icon={Link} kind="ghost" iconDescription="Add Reference" />
+					</div>
+					<Button icon={SendAlt} kind="primary">Send</Button>
+				</div>
 				<!-- Display dummy "posts" -->
-				{#each [1, 2, 3, 4, 5] as post}
+				<ClickableTile class="my-4">
+					<h4>Post 1</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+				</ClickableTile>
+				<ClickableTile class="my-4">
+					<h4>Post 2</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+					<!-- Insert 2 dummy pictures side-by-side, make them take up the entire width -->
+					<div class="flex gap-2 mt-4">
+						<img src="https://via.placeholder.com/250" alt="placeholder" class="w-1/2" />
+						<img src="https://via.placeholder.com/250" alt="placeholder" class="w-1/2" />
+					</div>
+					
+				</ClickableTile>
+				{#each [3, 4, 5] as post}
 					<ClickableTile class="my-4">
 						<h4>Post {post}</h4>
 						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
